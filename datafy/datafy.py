e9549be73c08089d3882b42d4ddcf687a7ac97d9
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-06-03 15:48:40 -0400

    Bugfix.

--- datafy/datafy.py
+++ datafy/datafy.py
@@ -156,7 +156,10 @@ def get(uri, sizeout=None, type_hints=(None, None), localized=False):
 
             # Recurse using the file driver to deal with local folders.
             ret = []
-            for filename in [name for name in z.namelist() if not os.path.isdir(name)]:
+            working_directory_abs_path = '{0}/{1}'.format(os.getcwd(), temp_foldername)
+            for filename in [name for name in z.namelist() if not os.path.isdir('{0}/{1}'.format(
+                    working_directory_abs_path, name)
+            )]:
                 ext = filename.split(".")[-1]
                 mime = magic.from_file('{0}/{1}'.format(temp_foldername, filename), mime=True)
 
