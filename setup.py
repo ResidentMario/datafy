63d64610b5512f390e5a69e3a22d0ddb28f4044c
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-08-12 15:18:56 -0400

    Increment version.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     packages = ['datafy'], # this must be the same as the name above
     install_requires=['requests', 'requests-file', 'python-magic'],
     py_modules=['datafy'],
-    version = '0.0.1',
+    version = '0.1.0',
     description = 'Read download URLs into datasets.',
     author = 'Aleksey Bilogur',
     author_email = 'aleksey.bilogur@gmail.com',
     url = 'https://github.com/ResidentMario/datafy',
-    download_url = 'https://github.com/ResidentMario/datafy/tarball/0.0.1',
+    download_url = 'https://github.com/ResidentMario/datafy/tarball/0.1.0',
     keywords = ['data', 'requests', 'data analysis', 'geospatial data', 'geospatial analytics', 'pandas'],
     classifiers = [],
 )
